f4c987dd7038c3e5455787817d903f9f0d75519c
Leah Arden <leaharden175@gmail.com>
2020-10-20 14:44:42 -0400

    sorting done

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.get('/apps', (req, res) => {
    
   const { genres, sort } = req.query;
 
+
+
+  if (sort === 'rating' || sort === 'app') {
+        sorting = sort.charAt(0).toUpperCase() + sort.slice(1);
+        playdata.sort((a,b) => {
+            return a[sorting] > b[sorting] ? 1: a[sorting]< b[sorting] ? -1: 0;
+        })
+    }
+
   
   let results = playdata
     .filter(appWord =>
@@ -23,7 +32,7 @@ app.get('/apps', (req, res) => {
         // .toLowerCase());
         // .includes(sort.toLowerCase()));
 
-
+/*
   if (sort === 'app' || sort === 'genre') {
     results
       .sort((a, b) => {
@@ -34,7 +43,7 @@ app.get('/apps', (req, res) => {
   } else { 
     results = "Error"  
   }
-
+*/
 
   res.json(results);
 
